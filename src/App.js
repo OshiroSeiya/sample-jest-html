452aac58039834b9cfc19edc5cd1dd3e2a6fda62
oshiro_seiya <oshiro_seiya@ca-adv.co.jp>
2017-09-13 17:51:23 +0900

    inline styleを使って文字を赤くした

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className="App-intro" style={{ color: 'red' }}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
