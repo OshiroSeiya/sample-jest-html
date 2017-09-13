6de5130ced69b1d164539645746042ffbbfc59c7
oshiro_seiya <oshiro_seiya@ca-adv.co.jp>
2017-09-13 17:38:30 +0900

    toMatchSnapshotを使ってSnapshotテスト追加

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('App Snapshot', () => {
+  const component = renderer.create(
+    <App />
+  );
+  expect(component).toMatchSnapshot('App is displayed?');
 });
